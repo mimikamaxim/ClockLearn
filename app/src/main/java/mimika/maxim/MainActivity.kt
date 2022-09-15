d4733d7e77e6746a21b6874889e456517bdcb249
Mimika <>
2022-09-16 00:03:18 +0500

    some test

--- app/src/main/java/mimika/maxim/MainActivity.kt
+++ app/src/main/java/mimika/maxim/MainActivity.kt
@@ -2,10 +2,14 @@ package mimika.maxim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.DigitalClock
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.training)
+        val time = findViewById<DigitalClock>(R.id.digit)
+        Log.e("AA", time.toString())
     }
 }
